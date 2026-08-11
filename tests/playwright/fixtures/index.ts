021252ec4fad583de4fa2e52637d4cd9709a7f2e
Ben Madley <91620321+ben-madley-mt@users.noreply.github.com>
2026-08-11 16:07:56 +0100

    Remove accidental changes

--- tests/playwright/fixtures/index.ts
+++ tests/playwright/fixtures/index.ts
@@ -1,5 +1,4 @@
-/
-  import { test as base, expect } from '@playwright/test';
+import { test as base, expect } from '@playwright/test';
 import { AxeBuilder } from '@axe-core/playwright';
 import { PageFactory } from '../pages/PageFactory.js';
 
